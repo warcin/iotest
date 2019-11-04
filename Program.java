ea148b93649b7ef5b4333336d9ddf86e0c933803
student <student@lab-44-5.cs.put.poznan.pl>
2019-11-04 14:21:16 +0100

    Drugikoment

--- Program.java
+++ Program.java
@@ -1,8 +1,10 @@
 //Pierwotna klasa!
+//Drugi komentarz
 public class MyClass {
   int z = 0;
   int x = 5;
   int y = 10;
+  int a = 111;
   public static void main(String[] args) {
     MyClass myObj = new MyClass();
 	System.out.println(myObj.y);
