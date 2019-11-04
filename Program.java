a4e038774b3942662b9d44d58d1c95ae725adfb9
student <student@lab-44-5.cs.put.poznan.pl>
2019-11-04 14:12:56 +0100

    zmianaY

--- Program.java
+++ Program.java
@@ -1,7 +1,7 @@
 public class MyClass {
   int z = 0;
   int x = 5;
-  int y = 1;
+  int y = 10;
   public static void main(String[] args) {
     MyClass myObj = new MyClass();
     System.out.println(myObj.x);
