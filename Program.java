c2b3e708fba889ebddc80dd633dca618c04d0d3c
student <student@lab-44-5.cs.put.poznan.pl>
2019-11-04 13:54:35 +0100

    Zmiennay

--- Program.java
+++ Program.java
@@ -1,8 +1,9 @@
 public class MyClass {
   int x = 5;
-
+  int y = 1;
   public static void main(String[] args) {
     MyClass myObj = new MyClass();
     System.out.println(myObj.x);
+	System.out.println(myObj.y);
   }
 }
